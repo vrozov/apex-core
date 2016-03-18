dd2962092371a2cf60b6c6cfa4e35ef250460606
Vlad Rozov <v.rozov@datatorrent.com>
2016-03-18 15:53:30 -0700

    Socket debug

--- bufferserver/src/main/java/com/datatorrent/bufferserver/client/Controller.java
+++ bufferserver/src/main/java/com/datatorrent/bufferserver/client/Controller.java
@@ -18,8 +18,6 @@
  */
 package com.datatorrent.bufferserver.client;
 
-import java.nio.channels.SocketChannel;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +26,6 @@ import com.datatorrent.bufferserver.packet.PurgeRequestTuple;
 import com.datatorrent.bufferserver.packet.ResetRequestTuple;
 import com.datatorrent.bufferserver.packet.Tuple;
 import com.datatorrent.bufferserver.util.Codec;
-import com.datatorrent.netlet.AbstractLengthPrependerClient;
 import com.datatorrent.netlet.util.Slice;
 
 /**
